ee60a311bf321c612c3fe12a4baff8061d7e8586
RAndriy1982 <randrew111222@gmail.com>
2023-02-20 16:32:32 +0000

    виправив скріпт на скролл при відкритому мобільному меню

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@
       openMenuBtn: document.querySelector("[data-menu-open]"),
       closeMenuBtn: document.querySelector("[data-menu-close]"),
       menu: document.querySelector("[data-menu]"),
-      body: document.querySelector("[body]"),
+      body: document.querySelector("body"),
       menuList: document.querySelector(".mob-menu-list")
     };
   
